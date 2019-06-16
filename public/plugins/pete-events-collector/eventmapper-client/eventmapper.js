81cf21da8907d5caa8258e798a2b9fe7c9163308
Dan Sturman <dan@sturman.org>
2019-06-15 20:09:27 -0700

    Make click-to-show the default behvaior.

--- public/plugins/pete-events-collector/eventmapper-client/eventmapper.js
+++ public/plugins/pete-events-collector/eventmapper-client/eventmapper.js
@@ -11,10 +11,10 @@ let markers = [];
 // This is a boolean flag used to open/close bubbles
 // automatically with mouse attention.  If false,
 // then we use click behavior to toggle visibility.
-// True is the default behavior.
+// False is the default behavior.
 // closeControl is the toggle button in the control
 // panel associated with managing this flag.
-let autoBubble = true;
+let autoBubble = false;
 let closeControl = null;
 
 // Utility function useful for printing loc structures
@@ -117,10 +117,10 @@ function mapEvents() {
 function toggleAutoBubble() {
     if (autoBubble) {
         autoBubble = false;
-        closeControl.style.backgroundColor = "#ccc";
+        closeControl.style.backgroundColor = "#fff";
     } else {
         autoBubble = true;
-        closeControl.style.backgroundColor = "#fff";
+        closeControl.style.backgroundColor = "#ccc";
     }
 }
 
@@ -189,7 +189,7 @@ function setupControl() {
     // Create global controls
     ShowControl(centerControlDiv, showAllMarkers, "Show All");
     ShowControl(centerControlDiv, showFutureEvents, "Upcoming Only");
-    closeControl = ShowControl(centerControlDiv, toggleAutoBubble, "DBG: AutoBubble");
+    closeControl = ShowControl(centerControlDiv, toggleAutoBubble, "DBG: Enable AutoBubble");
 }
 
 // drawMap() is called after events are parsed from JSON, then draws
@@ -241,8 +241,10 @@ function initMap() {
             rotateControl: false,
             styles: mapstyle,
         });
-    }).done(getEventJSON(function(response) {
-        eventList = JSON.parse(response);
-        drawMap();
-    }));
+        getEventJSON(function(response) {
+            eventList = JSON.parse(response);
+            drawMap();
+        });
+    })
+
 }
