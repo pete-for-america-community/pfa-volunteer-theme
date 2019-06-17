60574f9440133509c8df98749b86c11eb3c7aae7
Dan Sturman <dan@sturman.org>
2019-06-16 20:56:32 -0700

    First attempts to use a flexbox.

--- public/plugins/pete-events-collector/eventmapper-client/eventmapper.js
+++ public/plugins/pete-events-collector/eventmapper-client/eventmapper.js
@@ -227,7 +227,8 @@ function getEventJSON(callback) {
 // to continue with a parsed set of events by calling drawMap().
 function initMap() {
     $.getJSON("map-options.json", function (mapstyle) {
-        map = new google.maps.Map(document.getElementById("map"), {
+        let mElement = document.getElementById("map");
+        map = new google.maps.Map(mElement, {
             zoom: 5,
             center: {lat: 37.435851, lng: -122.133246},
             zoomControl: true,
