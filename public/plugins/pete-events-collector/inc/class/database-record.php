fc4a8b9007a2bfa20d6f1828ab6b485052be0891
jagp <jared.parmenter@gmail.com>
2019-06-22 17:34:38 -0700

    Processing datetimes as strings not unix timestamps

--- public/plugins/pete-events-collector/inc/class/database-record.php
+++ public/plugins/pete-events-collector/inc/class/database-record.php
@@ -48,16 +48,20 @@ class Event {
         // Determine if the api's original time format was UNIX or one of many string formats
         $unix_time = strtotime( $start_date );
         if ( $unix_time ) { 
-            $this->start_date = $unix_time;
+            $this->start_date = date( DATE_W3C, $unix_time );
         } else {
-            $this->start_date = $start_date;
+            $this->start_date = date( DATE_W3C, $start_date );
         }
         
-        $unix_time = strtotime( $end_date );
-        if ( $unix_time ) { 
-            $this->end_date = $unix_time;
+        if ( $end_date ) {
+            $unix_time = strtotime( $end_date );
+            if ( $unix_time ) { 
+                $this->end_date = date( DATE_W3C, $unix_time );
+            } else {
+                $this->end_date = date( DATE_W3C, $end_date );
+            }
         } else {
-            $this->end_date = $end_date;
+            $this->end_date = null;
         }
         
     }
